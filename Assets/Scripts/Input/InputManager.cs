af0d138649d5dcc624fcac9d477aa6df6f1d53cf
Markus Lange <markus@gamedevteacher.com>
2023-06-25 11:01:26 +0200

    Input Update

--- Assets/Scripts/Input/InputManager.cs
+++ Assets/Scripts/Input/InputManager.cs
@@ -1,15 +1,15 @@
 using UnityEngine;
 
-
 public class InputManager : MonoBehaviour
 {
     private Hypercamp_23 _inputAction;
-    
+
     public Vector2 Move { get; private set; }
     public bool Jump { get; private set; }
     public bool Interact { get; private set; }
+    public bool Sprint { get; private set; }
 
-    private void Start()
+    private void Awake()
     {
         _inputAction = new Hypercamp_23();
     }
@@ -19,9 +19,10 @@ public class InputManager : MonoBehaviour
         Move = _inputAction.Player.Move.ReadValue<Vector2>();
         Jump = _inputAction.Player.Jump.triggered;
         Interact = _inputAction.Player.Interact.triggered;
+        Sprint = _inputAction.Player.Sprint.inProgress;
+
     }
     
     private void OnEnable() => _inputAction.Enable();
-
     private void OnDisable() => _inputAction.Disable();
 }
