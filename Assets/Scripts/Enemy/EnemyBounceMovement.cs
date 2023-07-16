0e02782b3bf533cb78cc3250b7d5ac05c5ef3942
Markus Lange <markus@gamedevteacher.com>
2023-07-16 10:45:10 +0200

    Updates

--- Assets/Scripts/Enemy/EnemyBounceMovement.cs
+++ Assets/Scripts/Enemy/EnemyBounceMovement.cs
@@ -9,18 +9,25 @@ public class EnemyBounceMovement : MonoBehaviour
     public GameObject ForwardPoint;
     public LayerMask whatIsGround;
 
+    public bool flip;
+
     private void Start()
     {
+        flip = true;
         _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         _rigidbody2D.velocity = new Vector2(transform.localScale.x * moveSpeed, 0f);
+    }
 
+    private void LateUpdate()
+    {
         if (DetectWallOrFall())
         {
             transform.localScale = new Vector3(-transform.localScale.x, 1f, 1f);
+            flip = false;
         }
     }
 
@@ -30,7 +37,6 @@ public class EnemyBounceMovement : MonoBehaviour
                || (Physics2D.Raycast(transform.position, Vector2.right , 0.6f, whatIsGround) 
                    ||Physics2D.Raycast(transform.position, Vector2.left , 0.6f, whatIsGround)
                );*/
-        
         return !RayCaster(ForwardPoint.transform.position, Vector2.down) 
                || (RayCaster(Vector2.right) || RayCaster(Vector2.left));
     }
